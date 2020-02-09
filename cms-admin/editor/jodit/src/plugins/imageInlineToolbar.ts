722fc5b6d78f624920c57e7bf353f3ca397315c6
Md. Fakhrul Islam Talukder <fakhrulislamtalukder@gmail.com>
2020-02-09 18:38:49 +0600

    delete file

--- cms-admin/editor/jodit/src/plugins/imageInlineToolbar.ts
+++
@@ -1,9 +0,0 @@
-import Jodit from '../Jodit';
-
-export default function (editor: Jodit) {
-    editor.events.on('click', (event: Event) => {
-        if (event.target && event.target['tagName'] === 'IMG') {
-            //alert(1);
-        }
-    });
-};
